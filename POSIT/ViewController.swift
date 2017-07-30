f115afe29f0f3f8b73ad11c51db12a2f2f1f636e
Raphael Reyes <raphaelreyes@raphaels-macbook-pro.local>
2017-07-30 21:23:26 +0600

    Initial Commit

---
+++ POSIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POSIT
+//
+//  Created by Raphael Reyes on 30/07/2017.
+//  Copyright © 2017 Raphael Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
